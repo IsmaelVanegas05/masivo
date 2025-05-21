1381290b07b9c7edf70faab502a23e93da6c71ba
IsmaelVanegas05 <ismael.vanegas@es.uazuay.edu.ec>
2025-05-21 10:32:40 -0500

    1

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import plotly.express as px
 st.title("Mi segunda publicacón")
 st.write("Introducción")
 st.write("Esta es la primera que me sale algo")
-st.write("Esta es la primera que me sale algo")
+
+fig=px.bar(x=["A","B","C"],y=["2","5","6"])
+st.plotly_chart(fig)
