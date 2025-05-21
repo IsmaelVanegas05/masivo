0dca63569d01a22af454f61b53fb3e92b94b1aa3
IsmaelVanegas05 <ismael.vanegas@es.uazuay.edu.ec>
2025-05-21 10:22:40 -0500

    q

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Mi primera publicacón")
+st.title("Mi segunda publicacón")
 st.write("Introducción")
 st.write("Esta es la primera que me sale algo")
 st.write("Esta es la primera que me sale algo")
