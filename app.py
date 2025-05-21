6f20481e0f3f994abbdb0783d4f0ad2c10d1dafd
IsmaelVanegas05 <ismael.vanegas@es.uazuay.edu.ec>
2025-05-21 10:41:55 -0500

    figuras

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ st.write("Esta es la primera que me sale algo")
 
 fig=px.bar(x=["A","B","C"],y=["2","5","6"])
 st.plotly_chart(fig)
+
+fig2=px.histogram(x=["A","B","C"],y=["2","5","6"])
+st.plotly_chart(fig2)
+
+fig3= px.pie(names=["1","2","3"],values=["7","6","3"],
+            title="Distribuciones de Pesos en los Empleados")
+st.plotly_chart(fig3)
+
+fig4 = px.scatter(x=["1","2","3"],y=["7","6","3"], title="Correlacion de Peso y Edad")
+st.plotly_chart(fig4)
